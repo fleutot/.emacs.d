0061276204fe640df5935235c4cfd406a89ecf27
Gauthier Östervall <gao@bellman.se>
2013-08-29 10:04:36 +0200

    Ack smart case and recursive, backup files centralized.

--- init.el
+++ init.el
@@ -550,3 +550,13 @@ in any form without prior written permission from Bellman & Symfon AB.
 (prefer-coding-system 'utf-8)
 
 (setq-default fill-column 80)
+
+
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
