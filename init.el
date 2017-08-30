43229a63ff3ccd6e535d0d986971e6720c04fe61
Gauthier Östervall <gauthier.ostervall@lumenradio.com>
2017-08-30 16:09:14 +0200

    New theme bisque-darker, indent mode for Mira code

--- init.el
+++ init.el
@@ -126,12 +126,12 @@
  '(compile-command "make -k -j13")
  '(custom-safe-themes
    (quote
-    ("e6f803d7c8439ac1c2805fcc8a95a79250e129ce2d8f00344a0484b8ebc6ced2" "e15a578d4e2d8ef761019bc84b07bbe0642a36ea735626c35bb0f7dfae8b5f73" "3fd0fda6c3842e59f3a307d01f105cce74e1981c6670bb17588557b4cebfe1a7" "bfc3d92754a1f0af6bc3653b363f6cf3a0bd608f8a715a42e1bb416cc35e6b01" "36f3f367fc24b73917831203d0b2ef688e96da0315bccfe1a20cb48b8b6b2b7a" "d4b199a40ea2b66d80580ed16a9284244b34d62d6f85cc491e983c0870d6aad6" "b6d776913491a9ff9d24d893f72cf7d0054c079f646b2a9e5c915ce6b0f4fad7" "cfb6f9e7117a4cbc25b668ea5810d2e51795fca610ecdd5375af1e47f11a2d7b" "65426191cc767ef46550316aaedb390c964ff5106fb4915ced70d54b788585ee" "fd98fe5ad72432fde6d75fdaed432abab31d2020a584799f487fa2134554ba78" "6e4b0714df5cb9527106181f3ae7b3fd8d8dff6de5ddadbe8ef94e5b170185c7" "aef56024ff845bfe4ea2394247501d659ca45ddd28ae31982b29b1cb43e2b2f5" "067e5e21a9b8af74094c4c67f2468669c8e92743b19b658b458fa0146b6fe56e" "c90696c8d95e6f165ceeced9f35b200fe4c4c0eaff6dd6a0e6ea69f96ad200a9" "d64aea67b62b5219247814ff95026c1119bd328612b005fc9594dc22896735f6" "9b0fb159ce03afc785ebed71d80e2b3c64a99a2b616c293c9b16a1fbf85170d7" "4753abba0ea8b1214e9859beefa2448a8cb84cf0d6ff372d54faad67328adb59" "4905b46c5114003d08de39aa69f5171b6276c3562f17167f28322fb1dbbfc696" "be0a93110ce776fa2f1ab7cdc94ae3918ced4c57f365d994b4f5427b73f13e6e" "0917d7c62178351ead0f505ca97a4cef83b848d840701ccee24d8b2180efb920" "c728708eae31c8eec7a9d406e604bcb692f2727579fc154622b9c7a2a03fcbf6" "3b06158f1acc3f1602b2dc6f00c762e05219f600a296bbea2b02b2a10cc8e7f9" "6cf6b1d1bc1e0990b8236a7a3d22162722df470dfdd641f367a0ef1a66cb59af" "c40cd311cc4dac6a69a06cb58f626f41751378414f87660da8bb698e2035b653" "b1648b05c88763790f9ada18824bda2beb68744bedc280c3048194e874f1e2b1" "39c5fa26b66e6341dae664281ae9d951221ce15f82d1bb403307184b582d1ce5" "943c9fe92be754292152765052f554caf74e03a45596468108c71b9adfc526c2" "b58a22c0cf8a9fe905e306d9fac01a2ae1c742e3d0e8dcf63d789d539ccde0bc" "bf51e072aa61d3158147622e989fc8b719781daa1622304e9d4b9a77f8a04af6" "65734302cb6ca5d16191622c5767913e76f5da66d960c61ecfd4e9b091d9c398" "78d52aa5167b609a9163ea32bf8fb9e8304ae71e518bf20a6631d64a3bb405c4" "9273b34a1dddcf4b54a83e28751a37478e132f0d7a23eef6cf0a46e26fa20b35" "ac548cdf0d61acbc93f8bb6ea1eaec389de1466274414322dae6f47e6a96d774" "675fcf7e38164ca90e0583eaf131b442ecd07afb17abbe1c700c8ebc09469ea3" "c77a31867f444e1c82bacd22146cb2d781a471168305e1660558b2b54ec016b7" "01a269e63522c39b95bee8df829ae8633ea372fd1921487cd6ccac42b1bf1cb9" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" default)))
+    ("3913e7a00d03d30b128c78244586add22c8746bee6ea780aabd4da3776b8f8b3" "1574e7f585086ddc046786456430232322e2bbe453c07c8c4eeb04c000045937" "65dfecde0ade57145baf2dddc994122998df196956bc7a8567ac81693879c914" "f3dfabf0305c76140b0238ce663fff300cfc9e696b9e0a393d8bc2b27e431393" "3805745a0440d404f19fbdf82261ff176676220d37a29dbb1ef9758efa9362c7" "a665a17f0c0dcb626ce2140aaf6107ba43e95d75f5a00c1f617c252714cfd0cc" "8a9a43ff0c3e9da06da0fac51ecfe3520494144471734dcbb2f2448ea1ca6194" "1af8253e1e8aafa70ee72f3dd9b69604ea92412ff24c78fb1b6d1e341af55272" "f06add643615294e65d7c886bb7913432a1d6e59fd80e0e498fd81342c9057b3" "35068482c5374f259dcb7d3a2a6cfbfbd82ec12491e9130332711a38c4a78703" "fbadfaa790a5f0fc7b884781000236bcb87503a539f8c9571533de30bf29665b" "8decf4470bb3d30bc54c835bc7d3ed246cfde63d7a6267711e19f5f282cdf76d" "e6f803d7c8439ac1c2805fcc8a95a79250e129ce2d8f00344a0484b8ebc6ced2" "e15a578d4e2d8ef761019bc84b07bbe0642a36ea735626c35bb0f7dfae8b5f73" "3fd0fda6c3842e59f3a307d01f105cce74e1981c6670bb17588557b4cebfe1a7" "bfc3d92754a1f0af6bc3653b363f6cf3a0bd608f8a715a42e1bb416cc35e6b01" "36f3f367fc24b73917831203d0b2ef688e96da0315bccfe1a20cb48b8b6b2b7a" "d4b199a40ea2b66d80580ed16a9284244b34d62d6f85cc491e983c0870d6aad6" "b6d776913491a9ff9d24d893f72cf7d0054c079f646b2a9e5c915ce6b0f4fad7" "cfb6f9e7117a4cbc25b668ea5810d2e51795fca610ecdd5375af1e47f11a2d7b" "65426191cc767ef46550316aaedb390c964ff5106fb4915ced70d54b788585ee" "fd98fe5ad72432fde6d75fdaed432abab31d2020a584799f487fa2134554ba78" "6e4b0714df5cb9527106181f3ae7b3fd8d8dff6de5ddadbe8ef94e5b170185c7" "aef56024ff845bfe4ea2394247501d659ca45ddd28ae31982b29b1cb43e2b2f5" "067e5e21a9b8af74094c4c67f2468669c8e92743b19b658b458fa0146b6fe56e" "c90696c8d95e6f165ceeced9f35b200fe4c4c0eaff6dd6a0e6ea69f96ad200a9" "d64aea67b62b5219247814ff95026c1119bd328612b005fc9594dc22896735f6" "9b0fb159ce03afc785ebed71d80e2b3c64a99a2b616c293c9b16a1fbf85170d7" "4753abba0ea8b1214e9859beefa2448a8cb84cf0d6ff372d54faad67328adb59" "4905b46c5114003d08de39aa69f5171b6276c3562f17167f28322fb1dbbfc696" "be0a93110ce776fa2f1ab7cdc94ae3918ced4c57f365d994b4f5427b73f13e6e" "0917d7c62178351ead0f505ca97a4cef83b848d840701ccee24d8b2180efb920" "c728708eae31c8eec7a9d406e604bcb692f2727579fc154622b9c7a2a03fcbf6" "3b06158f1acc3f1602b2dc6f00c762e05219f600a296bbea2b02b2a10cc8e7f9" "6cf6b1d1bc1e0990b8236a7a3d22162722df470dfdd641f367a0ef1a66cb59af" "c40cd311cc4dac6a69a06cb58f626f41751378414f87660da8bb698e2035b653" "b1648b05c88763790f9ada18824bda2beb68744bedc280c3048194e874f1e2b1" "39c5fa26b66e6341dae664281ae9d951221ce15f82d1bb403307184b582d1ce5" "943c9fe92be754292152765052f554caf74e03a45596468108c71b9adfc526c2" "b58a22c0cf8a9fe905e306d9fac01a2ae1c742e3d0e8dcf63d789d539ccde0bc" "bf51e072aa61d3158147622e989fc8b719781daa1622304e9d4b9a77f8a04af6" "65734302cb6ca5d16191622c5767913e76f5da66d960c61ecfd4e9b091d9c398" "78d52aa5167b609a9163ea32bf8fb9e8304ae71e518bf20a6631d64a3bb405c4" "9273b34a1dddcf4b54a83e28751a37478e132f0d7a23eef6cf0a46e26fa20b35" "ac548cdf0d61acbc93f8bb6ea1eaec389de1466274414322dae6f47e6a96d774" "675fcf7e38164ca90e0583eaf131b442ecd07afb17abbe1c700c8ebc09469ea3" "c77a31867f444e1c82bacd22146cb2d781a471168305e1660558b2b54ec016b7" "01a269e63522c39b95bee8df829ae8633ea372fd1921487cd6ccac42b1bf1cb9" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" default)))
+ '(dtrt-indent-mode t nil (dtrt-indent))
  '(egg-enable-tooltip t)
  '(egg-git-command "git")
  '(fci-rule-color "#383838")
  '(fringe-mode (quote (1 . 1)) nil (fringe))
-; '(fuzzy-format-default-indent-tabs-mode t)
  '(global-hl-line-mode nil)
  '(indicate-empty-lines nil)
  '(magit-git-executable "git")
@@ -142,9 +142,9 @@
  '(safe-local-variable-values
    (quote
     ((eval when
-           (fboundp
-            (quote rainbow-mode))
-           (rainbow-mode 1)))))
+	   (fboundp
+	    (quote rainbow-mode))
+	   (rainbow-mode 1)))))
  '(send-mail-function (quote mailclient-send-it))
  '(sml/active-background-color "#E05A2B")
  '(sml/active-foreground-color "white")
@@ -235,7 +235,6 @@ kernel."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t (:background "#45423f" :foreground "#ac9a85"))))
  '(hl-line ((t (:inherit highlight :background "gray10"))))
  '(mode-line ((t (:background "#E05A2B" :foreground "white"))))
  '(mode-line-inactive ((t (:background "gray38" :foreground "gray45"))))
@@ -256,7 +255,7 @@ kernel."
     (color-theme-zenburn)
     ;(color-theme-pok-wog)
     )
-  (load-theme 'bisque t)
+  (load-theme 'bisque-darker t)
 )
 
 ;; no electric mode in c
@@ -305,7 +304,29 @@ kernel."
                         )))
   "Eclipse C Programming Style")
 (c-add-style "Eclipse C" eclipse-c-style)
-(setq c-default-style "Eclipse C")
+;(setq c-default-style "Eclipse C")
+
+(defconst mira-c-style
+  '((c-basic-offset . 4)
+    (indent-tabs-mode . nil)
+    (tab-width . 4)
+    (c-comment-only-line-offset . (0 . 0))
+    (c-offsets-alist . ((topmost-intro-cont    . 0)
+                        (statement-block-intro . +)
+                        (knr-argdecl-intro     . 5)
+                        (substatement-open     . +)
+                        (substatement-label    . +)
+                        (label                 . +)
+                        (statement-case-open   . +)
+			(statement-cont        . +)
+			(arglist-intro  . +)
+			(arglist-close  . 0)
+                        (access-label   . 0)
+                        (arglist-cont-nonempty . ++)
+                        )))
+  "Mira C Programming Style")
+(c-add-style "Mira C" mira-c-style)
+(setq c-default-style "Mira C")
 
 ;; Remove lull: scroll bar, tool bar, menu bar.
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
