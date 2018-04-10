5b0c384935e86f4152b6f823de9eecc9e91bd2a5
Gauthier Östervall <gauthier.ostervall@lumenradio.com>
2018-04-10 12:41:08 +0200

    ag, dumb-jump

---
+++ elpa/popup-20160709.729/popup-autoloads.el
@@ -0,0 +1,15 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("popup.el") (23228 35071 125437 807000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; popup-autoloads.el ends here
